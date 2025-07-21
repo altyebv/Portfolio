bdeb84038dc10341ab197f9dfd857546812d29b5
altyebv <altyeb.404@gmail.com>
2025-07-21 10:24:20 +0300

    fixed issues with the links

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,8 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import cocktail from "./cocktail.png";
+import iphone from "./iphone.png";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -25,6 +27,8 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+
+
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -59,4 +63,6 @@ export {
   carrent,
   jobit,
   tripguide,
+  cocktail,
+  iphone,
 };
